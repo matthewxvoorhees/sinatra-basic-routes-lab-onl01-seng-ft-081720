c505e1991f84304f1541de435023666ff8700dee
matthewxvoorhees <matthew.crnich@gmail.com>
2020-09-25 17:55:46 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Matt"
+      end
+
+      get '/hometown' do
+        "My hometown is Rose Park!"
+      end
+
+      get '/favorite-song' do
+        "My favorite song is The World Is Yours."
+    end
+
 end
